1101a755aa030d7680123d0a88dfde14bd6d33bd
Irina <neforsha@yandex.ru>
2022-11-25 18:53:15 +0300

    Comparisons with the Logical And Operator

---
+++ JS exercises (freeCodeCamp)/65.js
@@ -0,0 +1,15 @@
+function testLogicalAnd(val) {
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+      }
+    return "No";
+}
+  
+testLogicalAnd(0);
+testLogicalAnd(24);
+testLogicalAnd(25);
+testLogicalAnd(30);
+testLogicalAnd(50);
+testLogicalAnd(51);
+testLogicalAnd(75);
+testLogicalAnd(80);
