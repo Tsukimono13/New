08254273afb76b35431784d0684ef70de032be8b
Irina <neforsha@yandex.ru>
2022-11-26 02:35:02 +0300

    How many lightsabers do you own?

---
+++ JS (codewars)/4.js
@@ -0,0 +1,10 @@
+function howManyLightsabersDoYouOwn(name) {
+  let nameOfProg = "";
+    if (name == "Adam") {
+      return 0;
+    } else if ( name == "Zach") {
+      return 18;
+    } else {
+      return 0;
+    }
+}
