aea073bbe74e54776c893165e70a2a2ec5d4c91f
Irina <neforsha@yandex.ru>
2022-11-25 19:24:46 +0300

    Comparisons with the Logical Or Operator

---
+++ JS exercises (freeCodeCamp)/66.js
@@ -0,0 +1,15 @@
+function testLogicalOr(val) {
+    if (val > 20 || val < 10) {
+      return "Outside";
+    } 
+    return "Inside";
+  }
+  
+testLogicalOr(0);
+testLogicalOr(9);
+testLogicalOr(10);
+testLogicalOr(15);
+testLogicalOr(19);
+testLogicalOr(20);
+testLogicalOr(21);
+testLogicalOr(25);
