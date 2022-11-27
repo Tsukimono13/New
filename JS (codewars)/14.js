dba9fcdea2157e1d300a82d7e94fd26c8c43c77e
Irina <neforsha@yandex.ru>
2022-11-27 23:10:09 +0300

    Switch/Case - Bug Fixing #6

---
+++ JS (codewars)/14.js
@@ -0,0 +1,16 @@
+function evalObject(value){
+    var result = 0;
+    switch(value.operation){
+      case'+': result = value.a + value.b;
+        break;
+      case'-': result = value.a - value.b;
+        break;
+      case'/': result = value.a / value.b;
+        break;
+      case'*': result = value.a * value.b;
+        break;
+      case'%': result = value.a % value.b;
+        break;
+      case'^': result = Math.pow(value.a, value.b);
+        break;
+    }
