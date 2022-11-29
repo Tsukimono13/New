8e720ee80077c5aa6658774d17fba57364bac102
Irina <neforsha@yandex.ru>
2022-11-29 14:21:32 +0300

    Are You Playing Banjo?

---
+++ JS (codewars)/23.js
@@ -0,0 +1,6 @@
+function areYouPlayingBanjo(name) {
+    if (name.charAt(0) === 'r' || name.charAt(0) === 'R') {
+    return name + " plays banjo";
+    } 
+    return name + " does not play banjo";
+  }
