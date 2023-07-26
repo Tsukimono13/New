70ee0a2f276c051985c9aea598c2841d4da48d15
Irina Litvinova <neforsha@yandex.ru>
2023-07-26 19:43:05 +0300

    refactoring

--- JS (codewars)/1.js
+++ JS (codewars)/1.js
@@ -1,22 +1,26 @@
-function getLength(arr){
- 
+function getLength(arr) {
+
     return arr.length;
-  }
-  function getFirst(arr) {
+}
+
+function getFirst(arr) {
     //return the first element of arr
     return arr[0];
-  }
-  function getLast(arr){
+}
+
+function getLast(arr) {
     //return the last element of arr
     return arr[arr.length - 1];
-  }
-  function pushElement(arr){
-    var el=1;
+}
+
+function pushElement(arr) {
+    var el = 1;
     //push el to arr
     var push = arr.push(el);
     return arr;
-  }
-  function popElement(arr){
+}
+
+function popElement(arr) {
     //pop an element from arr
     const pop = arr.pop();
     return arr;
